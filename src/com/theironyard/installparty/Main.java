40b4bae9f3f0197d595beaabf110d85cb916292c
rabell66 <raabell66@gmail.com>
2017-07-26 14:30:29 -0500

    final

--- src/com/theironyard/installparty/Main.java
+++ src/com/theironyard/installparty/Main.java
@@ -1,7 +1,6 @@
 package com.theironyard.installparty;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -13,6 +12,9 @@ public class Main {
 
         List<Student> students = new ArrayList();
         List<Student> teachers = new ArrayList();
+        Set firstSet = new HashSet();
+        Set secondSet = new HashSet();
+        Set thirdSet = new HashSet();
 
         for (int i = 0; i < firstNames.length; i++) {
 
@@ -22,18 +24,35 @@ public class Main {
             student.setGrade("1");
             student.setId(i);
             students.add(student);
+            if(i<5){
+                 firstSet.add(student);
+            }
+            if(i>4 && i<10){
+                secondSet.add(student);
+
+            }
+            if(i>9){
+                thirdSet.add(student);}
 
 
 
-            if (i % 5 == 0) {
+            if ((i+1) % 5 == 0) {
             Student teacher = new Student();
             teacher.setFirstName(firstNames[i-1]);
-            teacher.setLastName((lastNames[i-2]));
+            teacher.setLastName((lastNames[i-3]));
             teacher.setGrade("1");
             teacher.setId(i);
             teachers.add(teacher);}
 
-        }
 
+        }
+        HashMap<Student, Set> classroom =new HashMap<>();
+
+            classroom.put(teachers.get(0),firstSet);
+            classroom.put(teachers.get(1),secondSet);
+            classroom.put(teachers.get(2),thirdSet);
+            System.out.println(classroom.values());
+            System.out.println(classroom.keySet());
+            System.out.printf("Classroom \n" + classroom.entrySet());
     }
-}
+    }
--- src/com/theironyard/installparty/Student.java
+++ src/com/theironyard/installparty/Student.java
@@ -2,44 +2,51 @@ package com.theironyard.installparty;
 
 public class Student {
 
-        private String firstName;
-        private String lastName;
-        private int id;
-        private String grade;
+    private String firstName;
+    private String lastName;
+    private int id;
+    private String grade;
 
 
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getGrade() {
-            return grade;
-        }
-
-        public void setGrade(String grade) {
-            this.grade = grade;
-        }
+    public String getFirstName() {
+        return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String toString() {
+        String output = String.format("'Name: %s %s, Grade: %s ID: %s", this.firstName,this.lastName, this.grade, this.id);
+        return output;
+
+    }
+
+}
 
 
 
