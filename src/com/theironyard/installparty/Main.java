df59c0d5b79bb511ec52788cc06d132febe13fb9
rabell66 <raabell66@gmail.com>
2017-08-04 08:03:20 -0500

    update

--- src/com/theironyard/installparty/Main.java
+++ src/com/theironyard/installparty/Main.java
@@ -11,12 +11,20 @@ public class Main {
                 "Evans", "Ferdinand", "Flintstone", "Ganders", "Garrison", "theDuck"};
 
         List<Student> students = new ArrayList();
-        List<Student> teachers = new ArrayList();
+        List<Teacher> teachers = new ArrayList();
         Set firstSet = new HashSet();
         Set secondSet = new HashSet();
         Set thirdSet = new HashSet();
 
         for (int i = 0; i < firstNames.length; i++) {
+            if ((i + 1) % 5 == 0) {
+                Teacher teacher = new Teacher();
+                teacher.setFirstName(firstNames[i - 1]);
+                teacher.setLastName((lastNames[i - 3]));
+                teacher.setGrade("1");
+                teacher.setId(i);
+                teachers.add(teacher);
+            }
 
             Student student = new Student();
             student.setFirstName(firstNames[i]);
@@ -24,35 +32,31 @@ public class Main {
             student.setGrade("1");
             student.setId(i);
             students.add(student);
-            if(i<5){
-                 firstSet.add(student);
+            if (i < 5) {
+                firstSet.add(students.get(i));
+            } else if (i < 10) {
+                secondSet.add(students.get(i));
+
+            } else {
+                thirdSet.add(student);
             }
-            if(i>4 && i<10){
-                secondSet.add(student);
-
-            }
-            if(i>9){
-                thirdSet.add(student);}
-
-
-
-            if ((i+1) % 5 == 0) {
-            Student teacher = new Student();
-            teacher.setFirstName(firstNames[i-1]);
-            teacher.setLastName((lastNames[i-3]));
-            teacher.setGrade("1");
-            teacher.setId(i);
-            teachers.add(teacher);}
 
 
         }
-        HashMap<Student, Set> classroom =new HashMap<>();
+        HashMap<Student, Set> classroom = new HashMap<>();
 
-            classroom.put(teachers.get(0),firstSet);
-            classroom.put(teachers.get(1),secondSet);
-            classroom.put(teachers.get(2),thirdSet);
-            System.out.println(classroom.values());
-            System.out.println(classroom.keySet());
-            System.out.printf("Classroom \n" + classroom.entrySet());
-    }
-    }
+        classroom.put(teachers.get(0), firstSet);
+        classroom.put(teachers.get(1), secondSet);
+        classroom.put(teachers.get(2), thirdSet);
+        for (Student e : classroom.keySet()) {
+            System.out.printf("Teachers:\n%s\n", e);
+
+        }
+        classroom.values().forEach(e -> System.out.printf("Students:\n%s\n", e));
+
+
+
+                  System.out.println("\n\n\n\nPrint out by entrySet" +classroom.entrySet());
+
+}
+                    }
